647e3e208e174d536a158b7e49373f3290cc384f
Parrot Live user <user@localhost.localdomain>
2022-03-06 08:19:02 +0000

    new UI design

--- client/src/App.js
+++ client/src/App.js
@@ -1,105 +1,28 @@
+import { ThemeProvider,createTheme } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Component/Navbar";
 import HomeScreen from "./Screen/Home";
-
+const theme=createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#0a1929',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+})
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Sci-Fi
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-mdb-toggle="collapse"
-              data-mdb-target="#navbarColor02"
-              aria-controls="navbarColor02"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarColor02">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item active">
-                  <a className="nav-link" aria-current="page" href="#">
-                    Blog
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Practice
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Leaderboard
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    About
-                  </a>
-                </li>
-              </ul>
-
-              <ul className="navbar-nav d-flex flex-row">
-                {/* <li className="nav-item me-3 me-lg-0">
-                  <a className="nav-link" href="#">
-                    <i className="fas fa-shopping-cart"></i>
-                  </a>
-                </li>
-                <li className="nav-item me-3 me-lg-0">
-                  <a className="nav-link" href="#">
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                </li> */}
-                <li className="nav-item me-3 me-lg-0 dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-mdb-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    <i className="fas fa-user"></i>
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<HomeScreen/>}/>
+          <Route path="/" element={<HomeScreen />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   );
 }
 
